d8e2b734b8805c41c466bec2745bc24f66cede66
CTL098775 <j747300k@gmail.com>
2021-03-29 04:13:19 +0800

    2HW

---
+++ 2HW/2HW.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _2HW
+{
+    public partial class _2HW : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int i_input = 66;
+            int i_sum = 0;
+            for (int i = 1; i <= i_input; i++)
+            {
+                if (i % 2 == 0 || i % 3 == 0)
+                {
+                    i_sum += i;
+                }
+            }
+            Response.Write(i_sum);
+            
+        }
+    }
+}
